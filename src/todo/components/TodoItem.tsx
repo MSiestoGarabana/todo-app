689efa1c7ce41828ce0adf21c9b2edbba61e0946
MSiesto <miguelsiesto@gmail.com>
2023-01-24 20:46:47 +0100

    implementing useTodos into TodoItem.tsx + handleDbCLick inline

--- src/todo/components/TodoItem.tsx
+++ src/todo/components/TodoItem.tsx
@@ -1,5 +1,6 @@
-import { useContext } from "react"
-import { TodoContext } from "../context/TodoContext"
+// import { useContext } from "react"
+// import { TodoContext } from "../context/TodoContext"
+import { useTodos } from "../hooks/useTodos"
 import { Todo } from "../interfaces/interfaces"
 
 interface todoItemProps {
@@ -8,17 +9,18 @@ interface todoItemProps {
 
 export const TodoItem = ({ todo }: todoItemProps) => {
 
-    const { toggleTodo } = useContext(TodoContext)
+   // const { toggleTodo } = useContext(TodoContext)
+   const { toggleTodo } = useTodos()
 
-    const handleDbClick = () => {
-        toggleTodo( todo.id );
-    }
+   // const handleDbClick = () => {
+   //     toggleTodo( todo.id );
+   // }
 
   return (
     <li style={{
       cursor: 'pointer',
       textDecoration: todo.completed ? 'line-through' : ''
-    }} onDoubleClick={handleDbClick}>
+    }} onDoubleClick={() => toggleTodo( todo.id )}>
         {todo.desc}
     </li>
   )
