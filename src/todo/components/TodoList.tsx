13f7e1133861bdfebef55af1522755738bdbf997
MSiesto <miguelsiesto@gmail.com>
2023-01-24 20:15:53 +0100

    Setting up TodoList.tsx

--- src/todo/components/TodoList.tsx
+++ src/todo/components/TodoList.tsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { TodoContext } from '../context/TodoContext'
+import { TodoItem } from './TodoItem'
 
 export const TodoList = () => {
   
+    const {todoState} = useContext (TodoContext)
+    const { todos } = todoState
   
     return (
     <ul>
-     
+     { 
+     todos.map( todo=> <TodoItem key = {todo.id} todo={ todo } /> )
+     }
     </ul>
   )
 }
